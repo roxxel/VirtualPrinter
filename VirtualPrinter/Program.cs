fb7671fbb9fc81396ce719311345e3ffc833a18c
Roxxel <warperthe@gmail.com>
2024-04-06 22:03:20 +0300

    Add project files.

---
+++ VirtualPrinter/Program.cs
@@ -0,0 +1,92 @@
+﻿using PdfiumViewer;
+using PrinterQueueWatch;
+using System.Drawing.Printing;
+using System.Text;
+using System.Threading.Channels;
+using VirtualPrinter;
+using WatsonWebsocket;
+
+var env = Environment.GetCommandLineArgs();
+
+if (env[1] == "server")
+{
+    new Server();
+    await Task.Delay(-1);
+}
+else
+{
+    try
+    {
+
+        WatsonWsClient client = new WatsonWsClient(env.ElementAtOrDefault(2) ?? "localhost", 56466, false);
+        client.ServerConnected += ServerConnected;
+        client.ServerDisconnected += ServerDisconnected;
+        client.MessageReceived += MessageReceived;
+        var success = client.StartWithTimeout(10);
+        while (!success)
+        {
+            Console.WriteLine("Couldn't connect. Retrying connection");
+            success = client.StartWithTimeout(10);
+        }
+        await Task.Delay(-1);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+
+    static async void MessageReceived(object sender, MessageReceivedEventArgs args)
+    {
+        try
+        {
+            var filename = new Random().Next().ToString();
+            File.WriteAllBytes(filename, args.Data.ToArray());
+            await Console.Out.WriteLineAsync($"Received file {filename}");
+            await PrintPdf(filename);
+            File.Delete(filename);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    static void ServerConnected(object sender, EventArgs args)
+    {
+        Console.WriteLine("Server connected");
+    }
+
+    static void ServerDisconnected(object sender, EventArgs args)
+    {
+        Console.WriteLine("Server disconnected. Trying to reconnect");
+        var client = (WatsonWsClient)sender;
+        var success = client.StartWithTimeout(10);
+        while (!success)
+        {
+            Console.WriteLine("Couldn't connect. Retrying connection");
+            success = client.StartWithTimeout(10);
+        }
+    }
+}
+
+static async Task PrintPdf(string filename)
+{
+    try
+    {
+        using PdfDocument doc = PdfDocument.Load(filename);
+
+        var printDoc = doc.CreatePrintDocument();
+        printDoc.PrinterSettings.PrinterName = Environment.GetCommandLineArgs().ElementAtOrDefault(3) ?? "HP Universal Printing PS";
+        printDoc.PrintController = new StandardPrintController();
+        await Console.Out.WriteLineAsync($"Sent {filename} for printing");
+        printDoc.Print();
+        await Console.Out.WriteLineAsync($"{filename} printed and deleted");
+        await Task.Delay(5000);
+
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+}
---
+++ VirtualPrinter/Server.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatsonWebsocket;
+
+namespace VirtualPrinter
+{
+    internal class Server
+    {
+        private WatsonWsServer server;
+
+        public Server()
+        {
+            server = new WatsonWsServer(Environment.GetCommandLineArgs().ElementAtOrDefault(2) ?? "127.0.0.1", 56466, false);
+            server.ClientConnected += ClientConnected;
+            server.ClientDisconnected += ClientDisconnected;
+            server.MessageReceived += MessageReceived;
+            Console.WriteLine($"Running on 56466");
+            server.Start();
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+
+                    var filename = Environment.GetCommandLineArgs().ElementAtOrDefault(3) ?? "C:\\pdf";
+                    Console.WriteLine($"listening {filename}");
+                    var watcher = new FileSystemWatcher(filename);
+                    watcher.NotifyFilter = NotifyFilters.LastWrite;
+                    watcher.EnableRaisingEvents = true;
+                    watcher.Filter = "*.*";
+                    watcher.Error += Watcher_Error;
+                    watcher.Changed += Watcher_Changed;
+                    await Task.Delay(-1);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            });
+        }
+
+        private async void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (File.GetAttributes(e.FullPath).HasFlag(FileAttributes.Directory))
+            {
+                return;
+            }
+            Console.WriteLine("new file");
+            var file = File.ReadAllBytes(e.FullPath);
+            foreach (var client in server.ListClients())
+            {
+                await server.SendAsync(client.Guid, file);
+            }
+            File.Delete(e.FullPath);
+        }
+
+        private void Watcher_Error(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine(e.GetException());
+        }
+
+        void ClientConnected(object sender, ConnectionEventArgs args)
+        {
+
+            Console.WriteLine("Client connected: " + args.Client.ToString());
+        }
+
+        void ClientDisconnected(object sender, DisconnectionEventArgs args)
+        {
+            Console.WriteLine("Client disconnected: " + args.Client.ToString());
+        }
+
+        void MessageReceived(object sender, MessageReceivedEventArgs args)
+        {
+            Console.WriteLine("Message received from " + args.Client.ToString() + ": " + Encoding.UTF8.GetString(args.Data));
+        }
+    }
+}
